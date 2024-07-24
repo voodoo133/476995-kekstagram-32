89160b7e8316eb08b821cb1aece5d6e3c9243efa
Anton <voodoo_133@mail.ru>
2024-07-24 11:41:57 +0300

    Дозагрузка комментариев

--- js/picture-modal/picture-modal.js
+++ js/picture-modal/picture-modal.js
@@ -2,8 +2,14 @@ import { getCommentsListFragment } from './comments.js';
 
 const modalEl = document.querySelector('.big-picture');
 const closeModalEl = modalEl.querySelector('#picture-cancel');
+const commentsLoadBtn = modalEl.querySelector('.comments-loader');
+const AMOUNT_COMMENTS_PER_CHUNK = 5;
+let modalComments = [];
+let commentsOffset = 0;
+
 
 const closePictureModal = () => {
+  commentsLoadBtn.classList.remove('hidden');
   modalEl.classList.add('hidden');
   document.body.classList.remove('modal-open');
 };
@@ -15,26 +21,40 @@ const onEscapeKeyDown = (e) => {
   }
 };
 
+const appendCommentsChunk = () => {
+  const commentsChunk = modalComments.slice(commentsOffset, commentsOffset + AMOUNT_COMMENTS_PER_CHUNK);
+
+  if (commentsChunk.length > 0) {
+    modalEl.querySelector('.social__comments').append(getCommentsListFragment(commentsChunk));
+  }
+
+  commentsOffset += Math.min(commentsChunk.length, AMOUNT_COMMENTS_PER_CHUNK);
+  modalEl.querySelector('.social__comment-shown-count').textContent = commentsOffset;
+
+  if (commentsOffset >= modalComments.length) {
+    commentsLoadBtn.classList.add('hidden');
+  }
+};
+
 const openPictureModal = (url, description, likes, comments) => {
+  commentsOffset = 0;
+  modalComments = comments;
+
   modalEl.querySelector('.big-picture__img img').src = url;
   modalEl.querySelector('.social__caption').textContent = description;
   modalEl.querySelector('.likes-count').textContent = likes;
-  modalEl
-    .querySelector('.social__comment-shown-count')
-    .textContent = modalEl.querySelector('.social__comment-shown-count').textContent;
+
   modalEl.querySelector('.social__comment-total-count').textContent = comments.length;
   modalEl.querySelector('.social__comments').innerHTML = '';
-  modalEl.querySelector('.social__comments').append(getCommentsListFragment(comments));
+  appendCommentsChunk();
 
-  modalEl.querySelector('.social__comment-count').classList.add('hidden');
-  modalEl.querySelector('.comments-loader').classList.add('hidden');
   modalEl.classList.remove('hidden');
-
   document.body.classList.add('modal-open');
 
   document.addEventListener('keydown', onEscapeKeyDown);
 };
 
+commentsLoadBtn.addEventListener('click', () => appendCommentsChunk());
 closeModalEl.addEventListener('click', () => closePictureModal());
 
 export { openPictureModal };
